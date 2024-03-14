36e223569cc1a2a3bbcdf194392066e63f49ac75
Int Fract <intfract@gmail.com>
2024-03-14 09:41:47 +0000

    added digit parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::fmt;
+use std::{any::Any, collections::HashMap, fmt};
+
+mod functions;
+use functions::add;
 
 struct Token {
     value: String,
@@ -17,7 +20,9 @@ struct TokenFactory {
     character: char,
     end: bool,
     letters: String,
+    digits: String,
     commutators: String,
+    comfns: HashMap<char, fn(Vec<f64>) -> f64>,
     keywords: Vec<String>,
 }
 
@@ -40,6 +45,15 @@ impl TokenFactory {
         word
     }
 
+    fn get_number(&mut self) -> String {
+        let mut number: String = "".to_string();
+        while !self.end && (self.digits.contains(self.character) || self.character == '.') {
+            number.push(self.character);
+            self.step();
+        }
+        number
+    }
+
     fn get_tokens(&mut self) -> Vec<Token> {
         let mut tokens: Vec<Token> = Vec::new();
         while !self.end {
@@ -50,6 +64,16 @@ impl TokenFactory {
                 } else {
                     tokens.push(Token { value: word, category: "REF".to_string() });
                 }
+            } else if self.commutators.contains(self.character) {
+                tokens.push(Token { value: self.character.to_string(), category: "COM".to_string() })
+            } else if self.digits.contains(self.character) || self.character == '+' || self.character == '-' {
+                let mut number = self.character.to_string();
+                number.push_str(&self.get_number());
+                if number.contains('.') {
+                    tokens.push(Token { value: self.character.to_string(), category: "FLT".to_string() })
+                } else {
+                    tokens.push(Token { value: self.character.to_string(), category: "INT".to_string() })
+                }
             }
             self.step();
         }
@@ -59,19 +83,23 @@ impl TokenFactory {
 
 fn create_token_factory(code: String) -> TokenFactory {
     let character = code.chars().nth(0).expect("blank code string detected");
+    let mut comfns: HashMap<char, fn(Vec<f64>) -> f64> = HashMap::new();
+    comfns.insert('+', add);
     TokenFactory {
         code,
         index: 0,
         character,
         end: false,
         letters: "abcdefghijklmnopqrstuvwxyz_".to_string(),
+        digits: "0123456789".to_string(),
         commutators: "+*=".to_string(),
-        keywords: vec!["declare".to_string()],
+        comfns,
+        keywords: vec!["declare".to_string(), "as".to_string(), "set".to_string(), "to".to_string()],
     }
 }
 
 fn main() {
-    let code = "declare x".to_string();
+    let code = "declare x\nset x to 1".to_string();
     let mut token_factory = create_token_factory(code);
     let tokens = token_factory.get_tokens();
     for token in &tokens {
